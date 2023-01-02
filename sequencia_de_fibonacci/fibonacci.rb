addadd6a8e05d3b553b13c97bc040f4c3bb05185
Ml-Rafa <rafael.aoliveira@mercadolivre.com>
2023-01-02 07:21:14 -0300

    add fibonacci logic

--- sequencia_de_fibonacci/fibonacci.rb
+++ sequencia_de_fibonacci/fibonacci.rb
@@ -1,5 +1,19 @@
-p 'Entre com um numero'
+p "Digite um numero"
 
-number = gets.strip
+number = gets.chomp.to_i
 
-p "seu numero foi #{number}"
+def fib(number)
+  number_one = 0
+  number_two = 
+  array = [0]
+
+  while number > 2
+    result =  number_one + number_two
+    array << result
+    number_one = number_two
+    number_two = result
+    number = number -1
+  end
+  array
+end
+p fib(number)
