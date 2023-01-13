ed93f4361cab6bace1293db7b671d6b6680f4051
Ml-Rafa <rafael.aoliveira@mercadolivre.com>
2023-01-13 15:54:32 -0300

    add coment equation fibonacci

--- sequencia_de_fibonacci/fibonacci.rb
+++ sequencia_de_fibonacci/fibonacci.rb
@@ -1,13 +1,14 @@
 p "Digite um numero"
+# Fn = Fn - 1 + Fn - 2
 
 number = gets.chomp.to_i
 
 def fib(number)
-  number_one = 0
-  number_two = 
+  number_one = 1
+  number_two = 0
   array = [0]
 
-  while number > 2
+  while number > 1
     result =  number_one + number_two
     array << result
     number_one = number_two
